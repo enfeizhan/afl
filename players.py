3db1a2e7aa19087c928fe349a8c99cf929f2f7fb
Fei Zhan <fei.zhan@jbadigital.com>
2015-06-30 22:38:21 +1000

    afl: add __init__.py version team and add more assert to players.py

--- players.py
+++ players.py
@@ -5,6 +5,8 @@ class Player(object):
     def __init__(self, first_name, surname):
         assert type(first_name) == str,\
             'First name must be of a string type!\n'
+        assert type(surname) == str,\
+            'Surname must be of a string type!\n'
         self.first_name = first_name 
         self.surname = surname
     def get_first_name(self):
---
+++ teams.py
@@ -0,0 +1,17 @@
+"""
+define team class
+"""
+
+class Team(object):
+    """
+    Class Team.
+    """
+    def __init__(self, name):
+        assert type(name) == str, 'Team name must be a string!'
+        self.name = name
+    def get_name(self):
+        return self.name
+    def __str__(self):
+        return self.name
+    def __repr__(self):
+        return self.name
