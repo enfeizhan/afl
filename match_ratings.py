542b302066df12280e7a2d1580aba393b3d7eedc
Fei Zhan <fei.zhan@jbadigital.com>
2015-06-30 23:27:20 +1000

    afl: add match_ratings.py get adjusted goals scored and get adjusted goals allowed for the first time

---
+++ match_ratings.py
@@ -0,0 +1,61 @@
+"""
+Match ratings: Adjusted Goal Score and Adjusted Goal Allowed
+"""
+
+
+def get_adj_goals_scored(
+        goals_scored,
+        opp_defensive,
+        avg_base=1.37,
+        ):
+    """
+    Get the adjusted goals scored.
+
+    Parameters
+    -----------
+    goals_scored : int
+        The raw goals scored by a team in a match.
+
+    opp_defensive : float
+        The opponent's defensive strength rating.
+
+    avg_base : float, default: 1.37
+        A constant indicating the average number of goals scored
+        per game in international competition
+        (about 1.37 goals per team per game).
+    """
+    adj_goals_scored = (
+        (goals_scored - opp_defensive)
+        / max(0.25, opp_defensive*0.424+0.548)
+        * (avg_base * 0.424 + 0.548)
+        + avg_base)
+    return adj_goals_scored
+
+
+def get_adj_goals_allowed(
+        goals_allowed,
+        opp_offensive,
+        avg_base=1.37,
+        ):
+    """
+    Get the adjusted goal allowed
+
+    Parameters
+    -----------
+    goals_allowed : int
+        The raw goals allowed by a team in a match.
+
+    opp_offensive : float
+        The opponent's offensive strength rating.
+
+    avg_base : float, default: 1.37
+        A constant indicating the average number of goals scored
+        per game in international competition
+        (about 1.37 goals per team per game).
+    """
+    adj_goals_allowed = (
+        (goals_allowed - opp_offensive)
+        / max(0.25, opp_offensive*0.424+0.548)
+        * (avg_base * 0.424 + 0.548)
+        + avg_base)
+    return adj_goals_allowed
